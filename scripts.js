c50fc756ca588a2c3dfa5f11c7e49196f9d19f5d
zuxeld <zuxeld@gmail.com>
2023-01-27 23:41:41 +0400

    +обр. раскр. списков меню

--- scripts.js
+++ scripts.js
@@ -15,17 +15,31 @@ function TypeFilter(argArray) {
 }
 
 // функция-генератор функций-обработчиков:
-function fActive_Gen(DOM_el, CSSclass, CSSclass_modifer, revers = true) {
-    if (revers) {
-        return function fActive() {
-            DOM_el.classList.add(String(CSSclass) + String(CSSclass_modifer));
-            console.log('обр соб');
+function createHendlerFun(DOM_el, CSSclass, CSSclass_modifer,  revers = false, minWinWidth=0, maxWinWidth=Infinity) {
+    if (revers === false) {
+        return function hendlerFun() {
+            if (window.innerWidth <= maxWinWidth && window.innerWidth > minWinWidth) {
+                DOM_el.classList.add(String(CSSclass) + String(CSSclass_modifer));
+                console.log('обр соб');
+            }
+        }
+    } else if (revers === true) {
+        return function hendlerFun() {
+            if (window.innerWidth <= maxWinWidth && window.innerWidth > minWinWidth) {
+                DOM_el.classList.remove(String(CSSclass) + String(CSSclass_modifer));
+                console.log('обр соб 2');
+            }
+        }
+    } else if (revers === 'toggle'){
+        return function hendlerFun() {
+            if (window.innerWidth <= maxWinWidth && window.innerWidth > minWinWidth) {
+                DOM_el.classList.toggle(String(CSSclass) + String(CSSclass_modifer));
+                console.log('обр соб 2');
+            }
         }
     } else {
-        return function fActive() {
-            DOM_el.classList.remove(String(CSSclass) + String(CSSclass_modifer));
-            console.log('обр соб 2');
-        }
+        console.error(`Not right value of <revers> parametr: needed
+        "true", "false" or "toggle" - expected "` + revers + '"')
     }
 }
 // ДОМ-элемент с нужным классом:
@@ -33,29 +47,10 @@ let CSSclass = "MainMenu__MenuItem--HoldMenuConteiner";
 let DOM_el = document.querySelectorAll('.'+CSSclass);
 console.log(DOM_el);
 for (let i = 0; i < DOM_el.length; i++) {
-    DOM_el[i].addEventListener("mouseenter", fActive_Gen(DOM_el[i], CSSclass,'--jsActive'));
-    DOM_el[i].addEventListener("mouseleave", fActive_Gen(DOM_el[i], CSSclass,'--jsActive',false));    
+    DOM_el[i].addEventListener("mouseenter", createHendlerFun(DOM_el[i], CSSclass,'--jsActive', false, 770));
+    DOM_el[i].addEventListener("mouseleave", createHendlerFun(DOM_el[i], CSSclass,'--jsActive', true, 770));  
+    DOM_el[i].addEventListener("click", createHendlerFun(DOM_el[i], CSSclass,'--jsActive', "toggle", 0, 770));
 }
 
 // проверка работы функции фильтрации:
 TypeFilter([['fghhj', 'string'],["true", 'boolean']])();
-
-// выпададение списоков меню при наведении мыши:
-// let elements = 
-// // document.onDOMContentLoaded = function() {
-//     for (let elem of document.querySelectorAll('.MainMenu__MenuItem--HoldMenuConteiner')) {
-//         elem.OnMouseOver = function() {
-//             holdedelem = elem.//querySelector('.MainMenu__HoldedMenuItems');
-//             console.log(holdedelem.innerHTML);
-//             holdedelem.style.visibility = "visible";
-//         }
-//         // elem.onmouseout = function() {
-//         //     elem.style.visibility = "hidden";
-//         // }
-//         // console.log("1");
-//         // elem.style.visibility = "visible";
-//         // console.log(elem.innerHTML);
-//     }
-// }
-// document.querySelectorAll('.MainMenu__MenuItem--HoldMenuConteiner')[0].style.visibility = "visible";
-// window.scrollX = 1;
